47d900200c8a6c3e31ebab9221072c7d8c5870ab
neale <nealeratzlaff@gmail.com>
2017-03-09 18:16:56 -0800

    added DRQN for pacman, and deleted files corresponding to old exploration of joint CNN RNN models

--- attention_based_text_summery/data/create_vocab.py
+++
@@ -1,6 +0,0 @@
-import re
-import collections
-words = re.findall('\w+', open('training-lewis.txt').read().lower())
-with open('vocab', 'wb') as f:
-  for k, v in collections.Counter(words).iteritems():
-    f.write(str(k) + " " + str(v)+'\n')
